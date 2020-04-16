0dffbb624f62aeafc8e3218bae280ff6a787109d
north <xiaobei.quq@gmail.com>
2020-04-16 16:24:12 +0800

    横向列表开发

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import Horizen from "../../baseUI/horizen-item";
+import { categoryTypes, alphaTypes } from "../../api/config";
+import { NavContainer } from "./style";
 
-function Rank(props) {
-  return  (
-    <div>Rank</div>
-  )
+function Singers() {
+  let [category, setCategory] = useState("");
+  let [alpha, setAlpha] = useState("");
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val);
+  };
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val);
+  };
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门):"}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+      ></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        handleClick={handleUpdateAlpha}
+        oldVal={alpha}
+      ></Horizen>
+    </NavContainer>
+  );
 }
 
-export default React.memo(Rank);
+export default React.memo(Singers);
